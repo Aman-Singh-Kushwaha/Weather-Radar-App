564225f4d9090f17edbbd7702de7cb975f8f397c
Aman-Singh-Kushwaha <amansinghkushwaha2003@gmail.com>
2023-02-05 16:27:18 +0530

    Minor CSS and Typo Improvements

--- app.js
+++ app.js
@@ -37,35 +37,12 @@ function renderDetails(info){  //For Updating Data in HTML
   weatherLogCity.innerHTML=`<h1>${info.location.name}, </h1>
                             <p>${info.location.region}, ${info.location.country}</p>
                             <h2>${info.current.temp_c}°C<h2>`;
-  weatherLogDetails.innerHTML=`<p>Feels Like : ${info.current.feelslike_c}</p>
+  weatherLogDetails.innerHTML=`<p>Feels Like: ${info.current.feelslike_c}°C</p>
                               <p>Humidity: ${info.current.humidity}%</p>
-                              <p>Clouds in Sky:${info.current.cloud}%</p>
-                              <p>Precipitation(in mm):${info.current.precip_mm}</p>
-                              <p>Pressure(in millibars):${info.current.pressure_mb}</p>
-                              <p>Wind Speed(mph): ${info.current.wind_mph} (${info.current.wind_dir})</p>`
+                              <p>Clouds: ${info.current.cloud}%</p>
+                              <p>Precipitation: ${info.current.precip_mm}mm</p>
+                              <p>Pressure: ${info.current.pressure_mb}mb</p>
+                              <p>Wind Speed: ${info.current.wind_mph}mph (${info.current.wind_dir})</p>`
   weatherCondition.innerHTML=`<h1>${info.current.condition.text}</h1>
-                              <img src="${info.current.condition.icon}" alt="Weather Coontion Icon">`;
+                              <img src="${info.current.condition.icon}" alt="Weather Condition Icon">`;
 }
-
-// document.getElementById('getApi').addEventListener('click', getApi);
-// const apiId= 'e2d2fe2c76f848a2b1570500222607';
-// function getApi() {
-//     var CityName = document.getElementById('cityName').value;
-// console.log(CityName)
-// const url = `http://api.weatherapi.com/v1/current.json?key=${apiId}&q=${CityName}&aqi=yes`;
-// console.log(url);
-// fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//         let output = `
-//             <div>
-//                 <div style="color:black; width:10rem; background-color:white; border-radius: 10px;" class="p-3">        
-//                     <p class="m-0" style="font-width:bold; font-size:2rem;">${data.location.name}</p>
-//                     <p class="m-0" style="font-width:bold; font-size:4rem;"> ${data.current.temp_c}<sup>°C</sup></p>
-//                 </div>
-//             </div>
-//             `;
-//         console.log('printing data', data);
-//         document.getElementById('output').innerHTML = output;
-//     }).catch((err) => console.log(err))
-// }
